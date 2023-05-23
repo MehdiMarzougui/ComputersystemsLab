1ed07045505f74dfb01ed641f32d898a2744c9d0
MehdiMarzougui <101800974+MehdiMarzougui@users.noreply.github.com>
2023-05-23 22:27:31 +0200

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -41,8 +41,7 @@ namespace LibraryManager
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
+
             if (rootFrame == null)
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
@@ -52,10 +51,8 @@ namespace LibraryManager
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: Load state from previously suspended application
                 }
 
-                // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
